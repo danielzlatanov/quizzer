ac6793c0e66263a1ded99c419137f3fd3df7045c
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-04-15 17:13:32 +0300

    Initial HTML scaffold done

--- src/app.js
+++ src/app.js
@@ -1,8 +1,43 @@
+import { page, render } from './lib.js';
+import { getUserData } from './util.js';
+
 const root = document.getElementById('content');
 
 // page(decorateCtx);
 
 // page('/', homePage);
+// page('/login', loginPage);
+// page('/register', registerPage);
+// page('/catalog', catalogPage);
+// page('/create', createPage);
+// page('/details/:id', detailsPage);
+// page('/edit/:id', editPage);
 
+setNav();
 // page.start();
 
+// function decorateCtx(ctx, next) {
+// 	ctx.render = content => render(content, root);
+// 	ctx.setNav = setNav;
+// 	next();
+// }
+
+function setNav() {
+	const userData = getUserData();
+	if (userData) {
+		document.querySelector('#user-nav').style.display = 'block';
+		document.querySelector('#guest-nav').style.display = 'none';
+	} else {
+		document.querySelector('#user-nav').style.display = 'none';
+		document.querySelector('#guest-nav').style.display = 'block';
+	}
+}
+
+// function onLogout() {
+// 	logout();
+// 	setNav();
+// 	page.redirect('/');
+// }
+
+import * as api from './api/data.js';
+window.api = api;
