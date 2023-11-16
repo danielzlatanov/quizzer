766ff8ade4fe2e5c4da46e96c399db1b07193bd6
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-11-16 19:04:19 +0200

    fix lib.js imports

--- src/lib.js
+++ src/lib.js
@@ -1,8 +1,8 @@
 import page from '//unpkg.com/page/page.mjs';
-import { html, render } from '//unpkg.com/lit-html?module';
-import { until } from '//unpkg.com/lit-html/directives/until?module';
-import { cache } from '//unpkg.com/lit-html/directives/cache?module';
-import { classMap } from '//unpkg.com/lit-html/directives/class-map?module';
-import { styleMap } from '//unpkg.com/lit-html/directives/style-map?module';
+import { html, render } from 'https://cdn.skypack.dev/lit-html@3.1.0';
+import { until } from 'https://cdn.skypack.dev/lit-html@3.1.0/directives/until';
+import { cache } from 'https://cdn.skypack.dev/lit-html@3.1.0/directives/cache';
+import { classMap } from 'https://cdn.skypack.dev/lit-html@3.1.0/directives/class-map';
+import { styleMap } from 'https://cdn.skypack.dev/lit-html@3.1.0/directives/style-map';
 
 export { page, html, render, until, cache, classMap, styleMap };
